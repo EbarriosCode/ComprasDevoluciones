57397f2d1a9220b5ee94b2a2f189e22dff658f17
EbarriosCode <guayoswing@gmail.com>
2017-04-17 23:44:01 -0600

    Creacion de procedimientos almacenados con transaccion para manejar las devolcuiones

--- DataBase/transaccion.sql
+++ DataBase/transaccion.sql
@@ -1,60 +1,7 @@
-DROP PROCEDURE sp_TransaccionVentas;
-CALL sp_TransaccionVentas(curdate(),'0001',1,1,2);
-
-
-DELIMITER $$
-CREATE PROCEDURE sp_TransaccionVentas
-(in Fecha date,in Documento varchar(50),in IdCliente int,in IdProducto int,in Cantidad int)
-BEGIN
-	 /*DECLARE EXIT HANDLER FOR SQLEXCEPTION
-	 BEGIN
-	 SHOW ERRORS LIMIT 1;
-	 ROLLBACK;
-	 END; 
-	 
-     DECLARE EXIT HANDLER FOR SQLWARNING
-	 BEGIN
-	 SHOW WARNINGS LIMIT 1;
-	 ROLLBACK;
-	 END;
-		
-	*/
-	START TRANSACTION;    
-		/* insertar en la tabla ventas 
-		INSERT INTO ventas(fecha,documento,idCliente) VALUES(Fecha,Documento,IdCliente);*/
-		SET @idVenta = (SELECT MAX(idVenta) FROM ventas);/*LAST_INSERT_ID();*/
-		select @idVenta as idVenta;
-        
-		SET @idProducto = IdProducto;
-        select @idProducto as idProducto;
-        
-		SET @PrecioProducto = (SELECT precio FROM productos WHERE idProducto = @idProducto);
-        select @PrecioProducto as precioProducto;
-        
-		SET @cantidad = Cantidad;
-        select @cantidad as cantidad;
-        
-		SET @costoTotal = (@cantidad*@PrecioProducto);
-		select @costoTotal as costoTotal;
-        
-		/* insertar el detalle en la tabla ventasdetalle 
-		INSERT INTO ventasdetalle(idVenta,idProducto,cantidad,precio,costoTotal)VALUES(@idVenta,@idProducto,@cantidad,@PrecioProducto,@costoTotal);
-		
-		/* descontar la existencia de la tabla productos 
-		UPDATE productos SET existencia = (existencia - @cantidad) WHERE idProducto = @idProducto;
-	
-    COMMIT; */
-END $$
-    
-
-truncate table ventasdetalle;
-
 select @@autocommit AS AUTO;
 show engines;
 
-
-
-
+-- procedimiento almacenado con transaccion para realizar una venta
 DELIMITER $$
 CREATE PROCEDURE sp_TransaccionVentas
 (IN Fecha date,IN IdCliente int,IN IdProducto int,IN Cantidad int)
@@ -97,24 +44,77 @@ END $$
 DROP PROCEDURE sp_TransaccionVentas;
 CALL sp_TransaccionVentas(curdate(),1,1,2);
 
-
 truncate table ventas;
 truncate table ventasdetalle;
-
 
 -- procedimiento que valida si existe una venta y devuelve la diferencia de dias de una devolucion y venta
 DELIMITER $$
 CREATE PROCEDURE sp_existeVenta_diferenciaFechas
-(IN documento INT(11),IN fechaHoy DATE,OUT diferencia INT(11))
+(IN documento INT(11),IN fechaHoy DATE,OUT diferenciaDias INT(11))
 BEGIN
 	
     SET @fechaVenta := (SELECT VF.fecha FROM ventas VF WHERE VF.idVenta=documento);
-	SET diferencia := DATEDIFF(fechaHoy,@fechaVenta); 
-    SELECT V.idVenta,V.fecha,diferencia FROM ventas V WHERE V.idVenta=documento;
-
+	SET diferenciaDias := DATEDIFF(fechaHoy,@fechaVenta); 
+    /*SELECT V.idVenta,V.fecha,diferencia FROM ventas V WHERE V.idVenta=documento;*/
+	SELECT V.idVenta,V.fecha,V.idCliente,C.nombreCliente,VD.idProducto,
+	   P.codigoProducto,P.nombreProducto,M.nombreMarca,P.descripcion,VD.precio,VD.cantidad,
+       VD.costoTotal,diferenciaDias
+	FROM ventas V 
+	INNER JOIN clientes C ON V.idCliente = C.idCliente
+	INNER JOIN ventasdetalle VD ON V.idVenta = VD.idVenta
+	INNER JOIN productos P ON VD.idProducto = P.idProducto
+	INNER JOIN marcaproductos M ON P.idMarca = M.idMarca
+	WHERE V.idVenta=documento;
 END $$
 
 DROP PROCEDURE sp_existeVenta_diferenciaFechas;
 CALL sp_existeVenta_diferenciaFechas(6,curdate(),@diferencia);
 
+-- procedimiento almacenado con transaccion para realizar una devolucion
+DELIMITER $$
+CREATE PROCEDURE sp_TransaccionDevoluciones
+(IN Fecha date,IN Documento int,IN IdProducto int,IN Cantidad int,IN IdCliente int)
+BEGIN
+	 DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	 BEGIN
+	 SHOW ERRORS LIMIT 1;
+	 ROLLBACK;
+	 END; 
+	 
+     DECLARE EXIT HANDLER FOR SQLWARNING
+	 BEGIN
+	 SHOW WARNINGS LIMIT 1;
+	 ROLLBACK;
+	 END;
+		
+	START TRANSACTION;    
+		/* insertar en la tabla devoluciones */
+		INSERT INTO devoluciones(fecha,idVenta) VALUES(Fecha,Documento);
+		SELECT @idDevolucion := MAX(idDevolucion) FROM devoluciones;
+		
+        SELECT @idProducto := IdProducto;
+		SELECT @precio := (SELECT P.precio FROM productos P WHERE P.idProducto = @idProducto);        
+		SELECT @cantidad := Cantidad;        
+		SELECT @costoTotal := (@cantidad*@precio);   
+                
+		 /*insertar el detalle en la tabla devolucionesdetalle */
+		INSERT INTO devolucionesdetalle(idDevolucion,idProducto,cantidad,precio,costoTotal,idCliente,impreso)
+                                  VALUES(@idDevolucion,@idProducto,@cantidad,@precio,@costoTotal,IdCliente,0);
+		
+        SELECT @existencia := (SELECT P.existencia FROM productos P WHERE P.idProducto = @idProducto);        
+        /*select if(@existencia < @cantidad,'No hay productos','Si seguir con la transaccion');*/
+        
+        
+		/* incrementar la existencia de la tabla productos */
+		UPDATE productos P SET P.existencia = P.existencia+@cantidad WHERE P.idProducto = @idProducto;
+        
+        /* cambiar el estado de impresoPagado en la tabla ventasdetale */
+		UPDATE ventasdetalle VD SET VD.impresoPagado = 2 WHERE VD.idVenta = Documento;
+    COMMIT; 
+END $$
+    
+DROP PROCEDURE sp_TransaccionDevoluciones;
+CALL sp_TransaccionDevoluciones(curdate(),2,3,2,3);
 
+truncate table devoluciones;
+truncate table devolucionesdetalle;
